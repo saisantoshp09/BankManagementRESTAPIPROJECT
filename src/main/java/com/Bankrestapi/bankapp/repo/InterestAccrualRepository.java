036015a95649a6ea4975e6c802780b5c7bb870fa
Prudhvi <saisantoshprudhvierraguntla@gmail.com>
2026-08-18 20:44:57 +0530

    Changes for InterestAccrualRepository

--- src/main/java/com/Bankrestapi/bankapp/repo/InterestAccrualRepository.java
+++ src/main/java/com/Bankrestapi/bankapp/repo/InterestAccrualRepository.java
@@ -2,10 +2,13 @@ package com.Bankrestapi.bankapp.repo;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.Bankrestapi.bankapp.entity.InterestAccrual;
 
 @Repository
 public interface InterestAccrualRepository extends JpaRepository<InterestAccrual, Long> {
-	List<InterestAccrual> findByAccountAccountNumber(Long accountNumber);
+	@Query("SELECT ia FROM InterestAccrual ia WHERE ia.account.account_number = :accountNumber")
+	List<InterestAccrual> findByAccountAccountNumber(@Param("accountNumber") Long accountNumber);
 }
